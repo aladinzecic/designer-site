243a642000ecbef995c4693b097bfbaf48050171
aladinzecic <aladinzecic10@gmail.com>
2023-09-23 23:50:34 +0200

    Loading page

--- designer-site/src/App.js
+++ designer-site/src/App.js
@@ -1,12 +1,21 @@
+import { useState,useEffect } from 'react';
 import './App.css';
 import Cursor from './components/Cursor/Cursor';
 import Illustration from './pages/Illustration/Illustration';
+import Loading from './pages/Loading/Loading';
 import MainPage from './pages/MainPage/MainPage';
 import WhatWeDo from './pages/WhatWeDo/WhatWeDo';
 function App() {
+  const [isLoading,setIsLoading]=useState(true);
+useEffect(()=>{
+setTimeout(()=>{
+  setIsLoading(false)
+},2000)
+},[])
   return (
-    <div className="App">
-      <Cursor/>
+    <div className={isLoading?"Loading":"App"} >
+      <Loading/>
+      {!isLoading&& <Cursor/>}
       <MainPage/>
       <Illustration/>
       <WhatWeDo/>
---
+++ designer-site/src/Context/AppContext.js
@@ -0,0 +1,6 @@
+import React, { createContext, useState } from "react";
+const AppContext = createContext();
+
+function ContextProvider({children}){
+    
+}
